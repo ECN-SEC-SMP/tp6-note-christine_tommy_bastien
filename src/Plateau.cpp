2de044edc107135d7b3feb14092e3eac690e096c
BastienFelix <159130564+BastienFelix@users.noreply.github.com>
2025-01-18 15:12:48 +0000

    Démarrage dév Plateau.cpp

--- src/Plateau.cpp
+++ src/Plateau.cpp
@@ -5,11 +5,11 @@ Plateau::Plateau(){
     vector<Case> vecteur_plateau = {
         Boulevard de Belleville; Rue Lecourbe;
         Rue de Vaugirard; Rue de Courcelles; Avenue de la République,;
-        Boulevard de la Villette; avenue de Neuilly; rue de Paradis;
+        Boulevard de la Villette; avenue de Neuilly; Rue de Paradis;
         Avenue Mozart; boulevard Saint-Michel; place Pigalle;
-        Avenue Matignon; boulevard Malesherbes; avenue Henri-Martin;
+        Avenue Matignon; boulevard Malesherbes; Avenue Henri-Martin;
         Faubourg Saint-Honoré; place de la Bourse; rue La Fayette;
-        Avenue de Breteuil; avenue Foch; boulevard des Capucines;
+        Avenue de Breteuil; Avenue Foch; boulevard des Capucines;
         Avenue des Champs-Élysées; rue de la Paix
     }
 
